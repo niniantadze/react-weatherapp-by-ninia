d4de26a9662b9e02afc685021f2b24371e60bd79
Nini Antadze <nini.antadze@gmail.com>
2024-01-22 16:55:59 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a href="https://www.linkedin.com/in/nino-antadze/" target="_target">
-          NiniA
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/niniantadze/react-weatherapp-by-ninia"
-          target="_target"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by{" "}
+          <a href="https://www.linkedin.com/in/nino-antadze/" target="_target">
+            NiniA
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/niniantadze/react-weatherapp-by-ninia"
+            target="_target"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
