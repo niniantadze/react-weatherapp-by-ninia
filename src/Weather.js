6ae04c9a98e17eed5fde8bae1ba3447afba5b638
Nini Antadze <nini.antadze@gmail.com>
2024-01-22 23:34:14 +0100

    mmade some changes for building error

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
