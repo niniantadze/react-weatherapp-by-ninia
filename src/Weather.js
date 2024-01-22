5986c103a7e49ca0f024edae9e78a2a1a2980b87
Nini Antadze <nini.antadze@gmail.com>
2024-01-22 19:08:37 +0100

    added basic style

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,6 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>New York</h1>
       <form>
         <div className="row">
           <div className="col-9">
@@ -12,26 +11,36 @@ export default function Weather() {
               type="search"
               placeholder="Search for the city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
-      </form>
+      </form>{" "}
+      <h1>New York</h1>
       <ul>
         <li>Monday 11:00</li>
         <li>Mostly sunny</li>
       </ul>
-      <div className="row">
-        <div className="col-6">
+      <div className="row mt-3">
+        <div className="col-md-6 d-flex align-items-center">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly sunny"
-          />{" "}
-          -2°C
+            className="mr-2"
+          />
+          <div>
+            <span className="temperature">-2</span>
+            <span className="unit"> °C</span>
+          </div>
         </div>
-        <div className="col-6">
+        <div className="col-md-6">
           <ul>
             <li>Precipitation: 0%</li>
             <li>Humidity: 54%</li>
